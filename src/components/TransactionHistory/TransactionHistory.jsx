03c9bb2f397d482d16020e543cd13051d91a0bd7
Katharinaad <vermilion.adams@gmail.com>
2023-09-24 21:29:14 +0200

    Update TransactionHistory.jsx

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,3 +1,31 @@
-// import css from './TransactionHistory.module.css';
+import css from './TransactionHistory.module.css';
 
-export function TransactionHistory() {}
+export function TransactionHistory({ items }) {
+  return (
+    <table className={css.transactionHistory}>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+      <tbody>
+        {items.map(item => (
+          <tr key={item.id}>
+            <td>{item.type}</td>
+            <td>{item.amount}</td>
+            <td>{item.currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+// {
+//     "id": "1e0700a2-5183-4291-85cc-2065a036a683",
+//     "type": "invoice",
+//     "amount": "964.82",
+//     "currency": "LRD"
+//   },
