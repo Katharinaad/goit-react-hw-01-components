24f76122d7ea401e2f7e319760c607b0a1147726
Katharinaad <vermilion.adams@gmail.com>
2023-09-24 19:26:21 +0200

    Update FriendListItem.jsx

---
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -0,0 +1,25 @@
+import css from './FriendListItem.module.css';
+
+export function FriendListItem({ friend }) {
+  return (
+    <li className={css.item} key={friend.id}>
+      <span
+        className={friend.isOnline ? css.statusOnline : css.statusOffline}
+      ></span>
+      <img
+        className={css.avatar}
+        src={friend.avatar}
+        alt="User avatar"
+        width="48"
+      />
+      <p className={css.name}>{friend.name}</p>
+    </li>
+  );
+}
+
+//  {
+//     "avatar": "https://cdn-icons-png.flaticon.com/512/1998/1998592.png",
+//     "name": "Mango",
+//     "isOnline": true,
+//     "id": 1812
+//   }
