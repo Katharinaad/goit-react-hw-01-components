a0846f0c2b57aa1a5264481cdd2904624a35af77
Katharinaad <vermilion.adams@gmail.com>
2023-09-26 08:37:24 +0200

    Update Profile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,6 +1,12 @@
 import css from './Profile.module.css';
 
-export function Profile({ username, tag, location, avatar, stats }) {
+export function Profile({
+  username,
+  tag,
+  location,
+  avatar,
+  stats: { followers, views, likes },
+}) {
   return (
     <div className={css.profile}>
       <div className={css.description}>
@@ -13,15 +19,15 @@ export function Profile({ username, tag, location, avatar, stats }) {
       <ul className={css.stats}>
         <li>
           <span className={css.label}>Followers</span>
-          <span className={css.quantity}>{stats.followers}</span>
+          <span className={css.quantity}>{followers}</span>
         </li>
         <li>
           <span className={css.label}>Views</span>
-          <span className={css.quantity}>{stats.views}</span>
+          <span className={css.quantity}>{views}</span>
         </li>
         <li>
           <span className={css.label}>Likes</span>
-          <span className={css.quantity}>{stats.likes}</span>
+          <span className={css.quantity}>{likes}</span>
         </li>
       </ul>
     </div>
