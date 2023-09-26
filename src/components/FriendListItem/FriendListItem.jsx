4ec215bb07ec133cd02918a4090242b6b1a7aa62
Katharinaad <vermilion.adams@gmail.com>
2023-09-26 09:33:44 +0200

    Update FriendListItem.jsx

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,18 +1,11 @@
 import css from './FriendListItem.module.css';
 
-export function FriendListItem({ friend }) {
+export function FriendListItem({ avatar, name, isOnline }) {
   return (
     <li className={css.item}>
-      <span
-        className={friend.isOnline ? css.statusOnline : css.statusOffline}
-      ></span>
-      <img
-        className={css.avatar}
-        src={friend.avatar}
-        alt="User avatar"
-        width="48"
-      />
-      <p className={css.name}>{friend.name}</p>
+      <span className={isOnline ? css.statusOnline : css.statusOffline}></span>
+      <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+      <p className={css.name}>{name}</p>
     </li>
   );
 }
